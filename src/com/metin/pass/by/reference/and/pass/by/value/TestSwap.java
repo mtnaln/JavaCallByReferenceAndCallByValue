f96b7f339ee867c1143603cbb7a09dc37a6b1bca
mtnaln <metinalniacik@gmail.com>
2018-09-22 08:32:11 +0300

    TestSwap class'ında düzenleme yapıldı.

--- src/com/metin/pass/by/reference/and/pass/by/value/TestSwap.java
+++ src/com/metin/pass/by/reference/and/pass/by/value/TestSwap.java
@@ -18,14 +18,14 @@ public class TestSwap {
         System.out.println(p2);
     }
 
-    private static void swap(Person p1, Person p2) {
+    private static void swap(Person p1Param, Person p2Param) {
 
-        Person temp = p1;
-        p1 = p2;
-        p2 = temp;
+        Person temp = p1Param;
+        p1Param = p2Param;
+        p2Param = temp;
 
         System.out.println("----------swap----------");
-        System.out.println(p1);
-        System.out.println(p2);
+        System.out.println(p1Param);
+        System.out.println(p2Param);
     }
 }
